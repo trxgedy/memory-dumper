e312cf704e09a2c71be18a64b76ffae50d1473a8
tragedy <79763373+trxgedy@users.noreply.github.com>
2024-03-02 22:16:10 -0300

    Add files via upload

---
+++ memory-dumper/memory-dumper/src/main.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.hpp"
+#include "dumper\dumper.hpp"
+
+int __stdcall main( )
+{
+	const auto dumper_ = std::make_unique<dumper::c_dumper>( );
+	dumper_->dump_memory( "lsass.exe" );	
+
+	return EXIT_FAILURE;
+}
